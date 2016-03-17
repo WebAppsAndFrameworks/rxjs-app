5d3d01f19fb42f994261312633828fb298e4204b
jessesh <jessesh@microsoft.com>
2016-03-17 13:14:01 -0700

    refactor methods

--- app/index.js
+++ app/index.js
@@ -7,50 +7,34 @@ var $ = require('jquery');
 var $adjective = $('#adjective'),
     $noun = $('#noun');
 
-var adjectiveUp = Rx.Observable.fromEvent($adjective, 'keyup')
-  .map(function (e) {
-    return e.target.value;
-  })
-  .filter(function (text) {
-    return text.length > 2;
-  })
-  .debounce(750)
-  .distinctUntilChanged();
+function observableFromInput(inputElement){
+  return Rx.Observable.fromEvent(inputElement, 'keyup')
+      .map(function (e) {
+        return e.target.value;
+      })
+      .filter(function (text) {
+        return text.length > 2;
+      })
+      .debounce(750);
+}
 
-var nounUp = Rx.Observable.fromEvent($noun, 'keyup')
-  .map(function (e) {
-    return e.target.value;
-  })
-  .filter(function (text) {
-    return text.length > 2;
-  })
-  .debounce(750)
-  .distinctUntilChanged();
-
-var comboUp = 
-  Rx.Observable.combineLatest(
-    adjectiveUp,
-    nounUp
-  );
-
+var adjectiveUp = observableFromInput($adjective);
+var nounUp = observableFromInput($noun);
 
 comboUp.subscribe(function(x) { console.log(x); });
 
-
-
-// var subscription; 
-// subscription = Rxhr.subscribe(
-//   function (data) {
-//     data.response.forEach(function (twt) {
-//       console.log(twt);
-//     });
-//   },
-//   function (err) {
-//     console.log(err);
-//     useTestData();
-//   }
-// );
-
+var subscription; 
+subscription = Rxhr.subscribe(
+  function (data) {
+    data.response.forEach(function (twt) {
+      console.log(twt);
+    });
+  },
+  function (err) {
+    console.log(err);
+    useTestData();
+  }
+);
 
 function useTestData(){
     var p = Promise.resolve(window.testData);
