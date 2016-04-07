1707e11e4e3462e21cfba7f4dc6e6bc0b0652d51
jessesh <jessesh@microsoft.com>
2016-04-06 17:53:03 -0700

    incremental update to testability

--- app/index.js
+++ app/index.js
@@ -10,6 +10,9 @@ var GIPHY = {
   apiKey: 'dc6zaTOxFJmzC'
 };
 
+var isTestEnviornment = window.location.search && window.location.search.indexOf("test") >= 0;
+var appElement = document.querySelector("#app");
+
 var $adjective = $('#adjective'),
     $noun = $('#noun');
 
@@ -21,10 +24,65 @@ var comboUp = Rx.Observable.combineLatest(
   nounUp
 );
 
-var isTestEnviornment = window.location.search && window.location.search.indexOf("test") >= 0;
-var fn = getMainCallback(isTestEnviornment);
 
-comboUp.subscribe(fn);
+
+comboUp.subscribe(
+  (x) => {
+    notifyLatestCombo(x);
+    if(!isTestEnviornment){
+      // Call the web API
+      searchGIPHY(x);
+    }
+  }
+);
+  
+function searchGIPHY(x) {
+  Rx.DOM.ajax({
+    url: GIPHY.searchUri + x.join('+') + '&api_key=' + GIPHY.apiKey,
+    responseType: 'json'
+  })
+  .subscribe(
+    function giphySuccess(results) {
+      var view = $('#view').html('');
+
+      results.response.data.forEach(function(item) {
+        var video = $('<video/>').appendTo(view);
+
+        videojs(video[0], {
+            'preload': 'auto',
+            'controls': true,
+            'loop': true
+          }, function() {
+            var player = this;
+            player.bigPlayButton.show();
+
+            player.on('pause', function() {
+              player.bigPlayButton.show();
+            });
+
+            player.on('play', function() {
+              player.bigPlayButton.hide();
+            });
+
+            video[0]
+              .parentNode
+              .classList.add('vjs-default-skin', 'vjs-big-play-centered');
+
+            player.src(item.images.fixed_width.mp4);
+        });
+      });
+    },
+    function giphyError(error) {
+      console.log(error);
+    }
+  );
+}
+
+function notifyLatestCombo(x) {
+  var event = document.createEvent('Event');  
+  event.initEvent('latestcombo', true, false);  
+  appElement.dispatchEvent(event);        
+}
 
 function observableFromInput(inputElement){
   return Rx.Observable.fromEvent(inputElement, 'keyup')
@@ -37,54 +95,7 @@ function observableFromInput(inputElement){
       .debounce(750);
 }
 
-function getMainCallback(isTestEnviornment){
-  if(isTestEnviornment){
-    return function test(x) {
-          console.log(x);
-        };
-  } else {
-    return function searchGIPHY(x) {
-      Rx.DOM.ajax({
-        url: GIPHY.searchUri + x.join('+') + '&api_key=' + GIPHY.apiKey,
-        responseType: 'json'
-      })
-      .subscribe(
-        function giphySuccess(results) {
-          var view = $('#view').html('');
 
-          results.response.data.forEach(function(item) {
-            var video = $('<video/>').appendTo(view);
-
-            videojs(video[0], {
-                'preload': 'auto',
-                'controls': true,
-                'loop': true
-              }, function() {
-                var player = this;
-                player.bigPlayButton.show();
-
-                player.on('pause', function() {
-                  player.bigPlayButton.show();
-                });
-
-                player.on('play', function() {
-                  player.bigPlayButton.hide();
-                });
-
-                video[0]
-                  .parentNode
-                  .classList.add('vjs-default-skin', 'vjs-big-play-centered');
-
-                player.src(item.images.fixed_width.mp4);
-            });
-          });
-        },
-        function giphyError(error) {
-          console.log(error);
-        });
-    };
-  }
-}
 
 // var subscription;
 // subscription = Rxhr.subscribe(
